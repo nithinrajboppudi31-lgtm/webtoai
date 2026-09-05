209ea8e6bf448f5cc9aa9e2afa2066792a0743c7
nithinrajboppudi31-lgtm <nithinrajboppudi31@gmail.com>
2026-09-05 15:16:11 +0530

    Rename import from ai.js to aiService.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import jwt from 'jsonwebtoken';
 import { Resend } from 'resend';
 import { PrismaClient } from '@prisma/client';
 import nodemailer from 'nodemailer';
-import { generateProjectCode, generateChatReply } from './ai.js';
+import { generateProjectCode, generateChatReply } from './aiService.js';
 
 dotenv.config();
 
